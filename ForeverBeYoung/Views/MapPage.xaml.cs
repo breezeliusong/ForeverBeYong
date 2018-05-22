c15a2041534a29c44826e2a51e036729bd2931a0
xiaosong zi <xiaosong zi>
2018-05-22 18:05:18 +0800

    start map page

---
+++ ForeverBeYoung/Views/MapPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ForeverBeYoung.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MapPage : Page
+    {
+
+        private Geolocator geolocator;
+        public MapPage()
+        {
+            this.InitializeComponent();
+
+
+
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+
+                     geolocator = new Geolocator { DesiredAccuracyInMeters = 0, DesiredAccuracy= PositionAccuracy.Default, ReportInterval= };
+
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+
+                    break;
+
+            }
+
+        }
+    }
+}
